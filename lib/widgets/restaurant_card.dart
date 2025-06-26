29ea080ce7ccb97eeeede084c18445b7bc535ce6
micheledicarlo <m.dicarlo10@studenti.unimol.it>
2025-06-26 22:46:48 +0200

    Add WidgetRestaurant

---
+++ lib/widgets/restaurant_card.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import '../models/restaurant.dart';
+import '../main.dart';
+
+class RestaurantCard extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: Replace with your actual widget implementation
+    return Container(child: Text('Restaurant Card'));
+  }
+}
