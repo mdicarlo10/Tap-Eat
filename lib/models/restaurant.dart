2a1ef4ba4cf6c1745a7aa3720d30fd7939d434b9
micheledicarlo <m.dicarlo10@studenti.unimol.it>
2025-06-28 10:34:39 +0200

    Changes restaurant.dart

--- lib/models/restaurant.dart
+++ lib/models/restaurant.dart
@@ -1,55 +1,39 @@
 class Restaurant {
-  final int? id;
   final String name;
-  final String distance;
-  final String type;
   final double latitude;
   final double longitude;
-  final String? address;
+  final String distance;
+  final String type;
   final String? imageUrl;
-  final double? rating;
-  final int timestamp;
 
   Restaurant({
-    this.id,
     required this.name,
-    required this.distance,
-    required this.type,
     required this.latitude,
     required this.longitude,
-    this.address,
+    required this.distance,
+    required this.type,
     this.imageUrl,
-    this.rating,
-    required this.timestamp,
   });
 
   factory Restaurant.fromMap(Map<String, dynamic> map) {
     return Restaurant(
-      id: map['id'],
-      name: map['name'],
-      distance: map['distance'],
-      type: map['type'],
-      latitude: map['latitude'],
-      longitude: map['longitude'],
-      address: map['address'],
+      name: map['name'] ?? '',
+      latitude: (map['latitude'] as num).toDouble(),
+      longitude: (map['longitude'] as num).toDouble(),
+      distance: map['distance'] ?? '',
+      type: map['type'] ?? '',
       imageUrl: map['imageUrl'],
-      rating: map['rating']?.toDouble(),
-      timestamp: map['timestamp'],
     );
   }
 
   Map<String, dynamic> toMap() {
     return {
-      'id': id,
       'name': name,
-      'distance': distance,
-      'type': type,
       'latitude': latitude,
       'longitude': longitude,
-      'address': address,
+      'distance': distance,
+      'type': type,
       'imageUrl': imageUrl,
-      'rating': rating,
-      'timestamp': timestamp,
     };
   }
 }
