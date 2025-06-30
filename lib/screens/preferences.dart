696eff433abd45b1be10d27914f7c002bf01eaca
micheledicarlo <m.dicarlo10@studenti.unimol.it>
2025-06-30 19:31:14 +0200

    change color

--- lib/screens/preferences.dart
+++ lib/screens/preferences.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import '../providers/restaurant_favorite_provider.dart';
 import '../widgets/restaurant_list.dart';
-import '../main.dart';
 
 class Preferences extends ConsumerWidget {
   const Preferences({Key? key}) : super(key: key);
@@ -10,28 +9,25 @@ class Preferences extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final allFavorites = ref.watch(favoritesProvider);
+    final colorScheme = Theme.of(context).colorScheme;
     return Scaffold(
-      backgroundColor: backgroundColor,
+      backgroundColor: colorScheme.surface,
       appBar: AppBar(
         title: const Text(
           'I tuoi preferiti',
-          style: TextStyle(
-            color: primaryColor,
-            fontSize: 20,
-            fontWeight: FontWeight.bold,
-          ),
+          style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
         ),
-        backgroundColor: backgroundColor,
+        backgroundColor: colorScheme.primary,
         elevation: 0,
         centerTitle: true,
-        foregroundColor: textColor,
+        foregroundColor: colorScheme.onPrimary,
       ),
       body:
           allFavorites.isEmpty
-              ? const Center(
+              ? Center(
                 child: Text(
                   'Nessun ristorante preferito',
-                  style: TextStyle(color: secondaryTextColor, fontSize: 18),
+                  style: TextStyle(color: colorScheme.secondary, fontSize: 18),
                 ),
               )
               : RestaurantList(
