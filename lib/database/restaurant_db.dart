48a7e72f72aa8743400e343af31a54b444bdb8c5
micheledicarlo <m.dicarlo10@studenti.unimol.it>
2025-06-30 18:03:49 +0200

    add timestamp

--- lib/database/restaurant_db.dart
+++ lib/database/restaurant_db.dart
@@ -20,7 +20,7 @@ class RestaurantDatabase {
     final dbPath = await getDatabasesPath();
     final path = join(dbPath, filePath);
 
-    return await openDatabase(path, version: 1, onCreate: _createDB);
+    return await openDatabase(path, version: 2, onCreate: _createDB);
   }
 
   Future _createDB(Database db, int version) async {
@@ -33,36 +33,64 @@ class RestaurantDatabase {
         distance TEXT NOT NULL,
         type TEXT NOT NULL,
         imageUrl TEXT,
+        timestamp INTEGER NOT NULL,
         isFavorite INTEGER NOT NULL DEFAULT 0
       )
     ''');
   }
 
   Future<void> insert(Restaurant restaurant) async {
-    final db = await instance.database;
+    final db = await database;
     await db.insert(
       'restaurants',
       restaurant.toMap(),
       conflictAlgorithm: ConflictAlgorithm.replace,
     );
+    await deleteOldestIfLimitExceeded();
   }
 
   Future<List<Restaurant>> getAll() async {
-    final db = await instance.database;
+    final db = await database;
     final maps = await db.query('restaurants', orderBy: 'id DESC');
 
     return maps.map((map) => Restaurant.fromMap(map)).toList();
   }
 
-  Future<List<Restaurant>> getHistory({int limit = 10}) async {
-    final db = await instance.database;
+  Future<List<Restaurant>> getHistory({int limit = 2}) async {
+    final db = await database;
     final maps = await db.query(
       'restaurants',
-      orderBy: 'id DESC',
+      orderBy: 'timestamp DESC',
       limit: limit,
     );
 
     return maps.map((map) => Restaurant.fromMap(map)).toList();
+  }
+
+  Future<void> deleteOldestIfLimitExceeded() async {
+    final db = await database;
+
+    final count = Sqflite.firstIntValue(
+      await db.rawQuery('SELECT COUNT(*) FROM restaurants'),
+    );
+
+    const int maxEntries = 50;
+
+    if (count != null && count > maxEntries) {
+      final numberToDelete = count - maxEntries;
+
+      await db.rawDelete(
+        '''
+        DELETE FROM restaurants 
+        WHERE id IN (
+          SELECT id FROM restaurants 
+          ORDER BY timestamp ASC 
+          LIMIT ?
+        )
+      ''',
+        [numberToDelete],
+      );
+    }
   }
 
   Future<void> updateFavoriteStatus(
@@ -79,7 +107,7 @@ class RestaurantDatabase {
   }
 
   Future<List<Restaurant>> getFavorites() async {
-    final db = await instance.database;
+    final db = await database;
     final maps = await db.query(
       'restaurants',
       where: 'isFavorite = ?',
